8945a112c757e099677daf94b62ad53d7b816ef4
PietroDanieli <1pie7d@gmail.com>
2023-01-05 21:59:36 +0100

    add img

--- js/javascript.js
+++ js/javascript.js
@@ -3,5 +3,8 @@ console.log("miao");
 for (i = 0; i < caffetteria.length; i++) {
     document.getElementById(
       "menù"
-    ).innerHTML += `<li>${caffetteria[i]["nome"]}</li>`;
+    ).innerHTML += `
+    <img class="img-fluid" src="${caffetteria[i]["img"]}" alt="${caffetteria[i]["nome"]}" />
+    <div>${caffetteria[i]["nome"]}</div>
+    `;
   }
--- js/menù_data.js
+++ js/menù_data.js
@@ -2,11 +2,13 @@ let caffetteria = [
     {
         nome : "caffè",
         prezzo : 1.20,
-        ingredienti : ["acqua","caffè"]
+        ingredienti : ["acqua","caffè"],
+        img : "img/caffè.jpeg"
     },
     {
         nome : "cappuccino",
         prezzo : 1.40,
-        ingredienti : ["acqua","caffè","latte"]
+        ingredienti : ["acqua","caffè","latte"],
+        img : "img/cappuccino.jpeg"
     }
 ]
