d44bbc02c870eeaee8ff9826ca58eeffde8a1add
PietroDanieli <1pie7d@gmail.com>
2023-01-05 21:48:47 +0100

    data menù

---
+++ js/menù_data.js
@@ -0,0 +1,12 @@
+let caffetteria = [
+    {
+        nome : "caffè",
+        prezzo : 1.20,
+        ingredienti : ["acqua","caffè"]
+    },
+    {
+        nome : "cappuccino",
+        prezzo : 1.40,
+        ingredienti : ["acqua","caffè","latte"]
+    }
+]
