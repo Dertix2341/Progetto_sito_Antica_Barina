7daadb9e3ab79c81709ef56ae4945e4616312378
PietroDanieli <1pie7d@gmail.com>
2023-01-05 23:54:45 +0100

    correct array and img value

--- js/menù_data.js
+++ js/menù_data.js
@@ -3,223 +3,223 @@ let caffetteria = [
         nome : "caffè/Macchiato",
         prezzo : 1.30,
         ingredienti : [],
-        img : "img/caffè.jpeg"
+        img :"img/caffè.jpeg"
     },
     {
         nome : "Macchiatone",
         prezzo : 1.50,
         ingredienti : [],
-        img : 
-    }
+        img :""
+    },
     {
         nome : "Cappucino",
         prezzo : 1.70,
         ingredienti : [],
-        img : 
-    }
+        img :""
+    },
     {
         nome : "Caffè Decaffeinato",
         prezzo : 1.40,
         ingredienti : [],
-        img : 
-    }
+        img :""
+    },
     {
         nome : "Macchiatone Decaffeinato",
         prezzo : 1.60,
         ingredienti : [],
-        img : 
-    }
+        img :""
+    },
     {
         nome : "Cappucino Decaffeinato",
         prezzo : 1.80,
         ingredienti : [],
-        img : 
-    }
+        img :""
+    },
     {
         nome : "Orzo/giseng",
         prezzo : 1.50,
         ingredienti : [],
-        img : 
-    }
+        img :""
+    },
     {
         nome : "Orzo/Giseng Macchitone",
         prezzo : 1.90,
         ingredienti : [],
-        img : 
-    }
+        img :""
+    },
     {
         nome : "Caffè Soia",
         prezzo : 1.60,
         ingredienti : [],
-        img : 
-    }
+        img :""
+    },
     {
         nome : "Macchiatone Soia",
         prezzo : 2.00,
         ingredienti : [],
-        img : 
-    }
+        img :""
+    },
     {
         nome : "Cappucino Soia",
         prezzo : 1.40,
         ingredienti : [],
-        img :
-    }
+        img :"",
+    },
     {
         nome : "Latte Macchiato",
         prezzo : 2.50,
         ingredienti : [],
-        img : 
-    }
+        img :""
+    },
     {
         nome : "Marocchino",
         prezzo : 1.70,
         ingredienti : [],
-        img : 
-    }
+        img :""
+    },
     {
         nome : "Caffè Americano",
         prezzo : 1.70,
         ingredienti : [],
-        img : 
-    }
+        img :""
+    },
     {
         nome : "Caffè con Panna",
         prezzo : 2.30,
         ingredienti : [],
-        img :
-    }
+        img :"",
+    },
     {
         nome : "Caffè Corretto",
         prezzo : 2.00,
         ingredienti : [],
-        img : 
-    }
+        img :""
+    },
     {
         nome : "Crema Caffè",
         prezzo : 2.80,
         ingredienti : [],
-        img : 
-    }
+        img :""
+    },
     {
         nome : "Crema caffe Grande",
         prezzo : 5.00,
         ingredienti : [],
-        img : 
-    }
+        img :""
+    },
     {
         nome : "Caffè Freddo",
         prezzo : 2.60,
         ingredienti : [],
-        img : 
-    }
+        img :""
+    },
     {
         nome : "Caffè shkerato",
         prezzo : 3.50,
         ingredienti : [],
-        img : 
-    }
+        img :""
+    },
     {
         nome : "Irish Coffee",
         prezzo : 5.00,
         ingredienti : [],
-        img : 
-    }
+        img :""
+    },
     {
         nome : "Cioccolatina",
         prezzo : 1.80,
         ingredienti : [],
-        img : 
-    }
+        img :""
+    },
     {
         nome : "Cioccolata con panna",
         prezzo : 4.00,
         ingredienti : [],
-        img : 
-    }
+        img :""
+    },
     {
         nome : "cioccolata con Rum/Brandy",
         prezzo : 4.50,
         ingredienti : [],
-        img : 
-    }
+        img :""
+    },
     {
         nome : "the/Cammomilla",
         prezzo : 3.20,
         ingredienti : [],
-        img : 
-    }
+        img :""
+    },
     {
         nome : "Vin Brulè",
         prezzo : 3.50,
         ingredienti : [],
-        img : 
-    }
+        img :""
+    },
     {
         nome : "Brioche Vuota",
         prezzo : 1.20,
         ingredienti : [],
-        img : 
-    }
+        img :""
+    },
     {
         nome : "Brioche Vegana",
         prezzo : 1.60/2.10,
         ingredienti : [],
-        img : 
-    }
+        img :""
+    },
     {
         nome : "Brioche Frcite",
         prezzo : 1.50/1.70,
         ingredienti : [],
-        img :
-    }
+        img :"",
+    },
     {
         nome : "Brioche doppia Frcitura",
         prezzo : 1.80/2.60,
         ingredienti : [],
-        img : 
-    }
+        img :""
+    },
     {
         nome : "Brioche Mignon Vuota",
         prezzo : 1.00,
         ingredienti : [],
-        img : 
-    }
+        img :""
+    },
     {
         nome : "Brioche Mignon Fatcita",
         prezzo : 1.20,
         ingredienti : [],
-        img : 
-    }
+        img :""
+    },
     {
         nome : "Gluten Free",
         prezzo : 2.50,
         ingredienti : [],
-        img :
-    }
+        img :"",
+    },
     {
         nome : "Paste",
         prezzo : 1.30/2.50,
         ingredienti : [],
-        img :
-    }
+        img :"",
+    },
     {
         nome : "Waffel",
         prezzo : 4.00,
         ingredienti : [],
-        img :
-    }
+        img :"",
+    },
     {
         nome : "Waffel con Nutella",
         prezzo : 5.00,
         ingredienti : [],
-        img :
-    }
+        img :"",
+    },
     {
         nome : "prendere da asporto",
         prezzo : 0.10,
         ingredienti : [],
-        img :
+        img :"",
     }
 ]
 let Bibite_e_Aperitivi =[
@@ -227,211 +227,211 @@ let Bibite_e_Aperitivi =[
         nome : "Bicchire d'acqua",
         prezzo : 0.70,
         ingredienti : [],
-        img :
-    }
+        img :"",
+    },
     {
         nome : "Bott. d'acqua 500ml",
         prezzo : 1.20,
         ingredienti : [],
-        img :
-    }
+        img :"",
+    },
     {
         nome : "Bott. d'acqua vetro 700ml",
         prezzo : 2.50,
         ingredienti : [],
-        img :
-    }
+        img :"",
+    },
     {
         nome : "Bibite in Lattina",
         prezzo : 2.50,
         ingredienti : [],
-        img :
-    }
+        img :"",
+    },
     {
         nome : "Succhi",
         prezzo : 3.00/3.50,
         ingredienti : [],
-        img :
-    }
+        img :"",
+    },
     {
         nome : "Spremuta",
         prezzo : 3.50,
         ingredienti : [],
-        img :
-    }
+        img :"",
+    },
     {
         nome : "Spremuta Mista",
         prezzo : 4.00,
         ingredienti : [],
-        img :
-    }
+        img :"",
+    },
     {
         nome : "Red Bull",
         prezzo : 4.00,
         ingredienti : [],
-        img :
-    }
+        img :"",
+    },
     {
         nome : "Birra in Bottiglia",
         prezzo : 3.50,
         ingredienti : [],
-        img :
-    }
+        img :"",
+    },
     {
         nome : "Birra Artigianale/Estera",
         prezzo : 4.00,
         ingredienti : [],
-        img :
-    }
+        img :"",
+    },
     {
         nome : "Chinotto",
         prezzo : 3.50,
         ingredienti : [],
-        img :
-    }
+        img :"",
+    },
     {
         nome : "Ginger Beer",
         prezzo : 3.50,
         ingredienti : [],
-        img :
-    }
+        img :"",
+    },
     {
         nome : "Tonica",
         prezzo : 3.50,
         ingredienti : [],
-        img :
-    }
+        img :"",
+    },
     {
         nome : "Spriz",
         prezzo : 3.50,
         ingredienti : [],
-        img :
-    }
+        img :"",
+    },
     {
         nome : "Hugo",
         prezzo : 4.00,
         ingredienti : [],
-        img :
-    }
+        img :"",
+    },
     {
         nome : "Crodino/Gingerino",
         prezzo : 3.50,
         ingredienti : [],
-        img :
-    }
+        img :"",
+    },
     {
         nome : "Campari Soda/Sanbitter",
         prezzo : 3.50,
         ingredienti : [],
-        img :
-    }
+        img :"",
+    },
     {
         nome : "Americano",
         prezzo : 6.00,
         ingredienti : [],
-        img :
-    }
+        img :"",
+    },
     {
         nome : "Negroni",
         prezzo : 7.00,
         ingredienti : [],
-        img :
-    }
+        img :"",
+    },
     {
         nome : "Calice di pros. D.O.C.",
         prezzo : 3.50,
         ingredienti : [],
-        img :
-    }
+        img :"",
+    },
     {
         nome : "Calice di pros. Rosè",
         prezzo :4.00,
         ingredienti : [],
-        img :
-    }
+        img :"",
+    },
     {
         nome : "Calice di pros. D.O.C.G.",
         prezzo : 4.00,
         ingredienti : [],
-        img :
-    }
+        img :"",
+    },
     {
         nome : "Calice Vino Bianco",
         prezzo : 3.50,
         ingredienti : [],
-        img :
-    }
+        img :"",
+    },
     {
         nome : "Calice Vino Rosso",
         prezzo : 3.50,
         ingredienti : [],
-        img :
-    }
+        img :"",
+    },
     {
         nome : "Bottiglia pros. D.O.C.",
         prezzo : 24.00,
         ingredienti : [],
-        img :
-    }
+        img :"",
+    },
     {
         nome : "Bottiglia pros. Rosè",
         prezzo : 26.00,
         ingredienti : [],
-        img :
-    }
+        img :"",
+    },
     {
         nome : "Bottigli pros. D.O.C.G.",
         prezzo : 28.00,
         ingredienti : [],
-        img :
-    }
+        img :"",
+    },
     {
         nome : "Boglia Vino Rosso",
         prezzo : 18.00,
         ingredienti : [],
-        img :
-    }
+        img :"",
+    },
     {
         nome : "Bottiglia Vino Bianco",
         prezzo : 18.00,
         ingredienti : [],
-        img :
-    }
+        img :"",
+    },
     {
         nome : "Cocchino",
         prezzo : 2.00,
         ingredienti : [],
-        img :
-    }
+        img :"",
+    },
     {
         nome : "Coca-Cola Piccola",
         prezzo : 2.50,
         ingredienti : [],
-        img :
-    }
+        img :"",
+    },
     {
         nome : "Birrino",
         prezzo : 2.30,
         ingredienti : [],
-        img :
-    }
+        img :"",
+    },
     {
         nome : "Birra Piccola 0,20cl",
         prezzo : 2.70,
         ingredienti : [],
-        img :
-    }
+        img :"",
+    },
     {
         nome : "Birra Media 0,30cl",
         prezzo : 3.30,
         ingredienti : [],
-        img :
-    }
+        img :"",
+    },
     {
         nome : "Birra Grande 0.40cl",
         prezzo : 4.50,
         ingredienti : [],
-        img :
+        img :"",
     }
     
 ]
@@ -440,49 +440,49 @@ let Cocktail, Distillati_e_Amari = [
         nome : "Cocktail",
         prezzo : 8.00,
         ingredienti : [],
-        img :
-    }
+        img :"",
+    },
     {
         nome : "Cocktail Analcolico",
         prezzo : 6.50,
         ingredienti : [],
-        img :
-    }
+        img :"",
+    },
     {
         nome : "Grappa",
         prezzo : 3.00,
         ingredienti : [],
-        img :
-    }
+        img :"",
+    },
     {
         nome : "Grappa di Pros/Barrique",
         prezzo : 3.50,
         ingredienti : [],
-        img :
-    }
+        img :"",
+    },
     {
         nome : "Brandy",
         prezzo : 3.50,
         ingredienti : [],
-        img :
-    }
+        img :"",
+    },
     {
         nome : "Amari",
         prezzo : 3.50,
         ingredienti : [],
-        img :
-    }
+        img :"",
+    },
     {
         nome : "Whishy/Bourbon",
         prezzo : 4.50,
         ingredienti : [],
-        img :
-    }
+        img :"",
+    },
     {
         nome : "Shot",
         prezzo : 3.00,
         ingredienti : [],
-        img :
+        img :"",
     }
 ]
 let Gastronomia = [
@@ -490,127 +490,127 @@ let Gastronomia = [
         nome : "Tramezzini",
         prezzo : 2.40,
         ingredienti : [],
-        img :
-    }
+        img :"",
+    },
     {
         nome : "Toast Classico",
         prezzo : 4.00,
         ingredienti : [],
-        img :
-    }
+        img :"",
+    },
     {
         nome : "Toast Farcito",
         prezzo : 4.50,
         ingredienti : [],
-        img :
-    }
+        img :"",
+    },
     {
         nome : "Hot Dog",
         prezzo : 4.00,
         ingredienti : [],
-        img :
-    }
+        img :"",
+    },
     {
         nome : "Cicchetti",
         prezzo : 2.00,
         ingredienti : [],
-        img :
-    }
+        img :"",
+    },
     {
         nome : "Tranci pizza Margherita",
         prezzo : 3.00,
         ingredienti : [],
-        img :
-    }
+        img :"",
+    },
     {
         nome : "Trancio di Pizza Farcita",
         prezzo : 3.50,
         ingredienti : [],
-        img :
-    }
+        img :"",
+    },
     {
         nome : "Panini Farciti",
-        prezzo : 5,50,
+        prezzo : 5.50,
         ingredienti : [],
-        img :
-    }
+        img :"",
+    },
     {
         nome : "Rosticeria",
         prezzo : 1.00/3.50,
         ingredienti : [],
-        img :
-    }
+        img :"",
+    },
     {
         nome : "Mozzarella in Carrozza",
         prezzo : 2.80,
         ingredienti : [],
-        img :
-    }
+        img :"",
+    },
     {
         nome : "Pattatine Fritte",
         prezzo : 3.80,
         ingredienti : [],
-        img :
-    }
+        img :"",
+    },
     {
         nome : "Ketchup/Maionese",
         prezzo : 0.50,
         ingredienti : [],
-        img :
-    }
+        img :"",
+    },
     {
         nome : "Primi Piatti",
         prezzo : 8.00,
         ingredienti : [],
-        img :
-    }
+        img :"",
+    },
     {
         nome : "Insalatona Semplice",
         prezzo : 7.00,
         ingredienti : [],
-        img :
-    }
+        img :"",
+    },
     {
         nome : "Tonno",
         prezzo : 3.50,
         ingredienti : [],
-        img :
-    }
+        img :"",
+    },
     {
         nome : "Gammberetti",
         prezzo : 3.50,
         ingredienti : [],
-        img :
-    }
+        img :"",
+    },
     {
         nome : "tacchino",
         prezzo : 2.50,
         ingredienti : [],
-        img :
-    }
+        img :"",
+    },
     {
         nome : "Sfilacci",
         prezzo : 2.50,
         ingredienti : [],
-        img :
-    }
+        img :"",
+    },
     {
         nome : "Olive",
         prezzo : 2.00,
         ingredienti : [],
-        img :
-    }
+        img :"",
+    },
     {
         nome : "Mais",
         prezzo : 2.00,
         ingredienti : [],
-        img :
-    }
+        img :"",
+    },
     {
         nome : "Uovo",
         prezzo : 2.00,
         ingredienti : [],
-        img :
+        img :"",
     }
     
 ]
@@ -619,91 +619,91 @@ let Apericena =[
         nome : "Tgliere MIni",
         prezzo : 7.00,
         ingredienti : [],
-        img :
-    }
+        img :"",
+    },
     {
         nome : "Tagliere per 1 persone",
         prezzo : 12.00,
         ingredienti : [],
-        img :
-    }
+        img :"",
+    },
     {
         nome : "Tagliere per 2 persone",
         prezzo : 21.00,
         ingredienti : [],
-        img :
-    }
+        img :"",
+    },
     {
         nome : "Tagliere per 4 persone",
         prezzo : 38.00,
         ingredienti : [],
-        img :
-    }
+        img :"",
+    },
     {
         nome : "Tagliere per 6 persone",
         prezzo : 50.00,
         ingredienti : [],
-        img :
-    }
+        img :"",
+    },
     {
         nome : "Tgliere MIni",
         prezzo : 7.00,
         ingredienti : [],
-        img :
-    }
+        img :"",
+    },
     {
         nome : "Tagliere per 1 persone",
         prezzo : 12.00,
         ingredienti : [],
-        img :
-    }
+        img :"",
+    },
     {
         nome : "Tagliere per 2 persone",
         prezzo : 21.00,
         ingredienti : [],
-        img :
-    }
+        img :"",
+    },
     {
         nome : "Tagliere per 4 persone",
         prezzo : 38.00,
         ingredienti : [],
-        img :
-    }
+        img :"",
+    },
     {
         nome : "Tagliere per 6 persone",
         prezzo : 50.00,
         ingredienti : [],
-        img :
-    }
+        img :"",
+    },
     {
         nome : "Tgliere MIni",
         prezzo : 7.00,
         ingredienti : [],
-        img :
-    }
+        img :"",
+    },
     {
         nome : "Tagliere per 1 persone",
         prezzo : 12.00,
         ingredienti : [],
-        img :
-    }
+        img :"",
+    },
     {
         nome : "Tagliere per 2 persone",
         prezzo : 21.00,
         ingredienti : [],
-        img :
-    }
+        img :"",
+    },
     {
         nome : "Tagliere per 4 persone",
         prezzo : 38.00,
         ingredienti : [],
-        img :
-    }
+        img :"",
+    },
     {
         nome : "Tagliere per 6 persone",
         prezzo : 50.00,
         ingredienti : [],
-        img :
+        img :"",
     }
     
 ]
