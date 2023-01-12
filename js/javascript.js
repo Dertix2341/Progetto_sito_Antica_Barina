e552a1e386dfb54c6d5d7f1ab1fd955e8b169a36
Dertix2341 <dentino2416@gmail.com>
2023-01-12 22:02:53 +0100

    I add a background image end a title inside

--- js/javascript.js
+++ js/javascript.js
@@ -1,10 +1,20 @@
 console.log("miao");
 
 for (i = 0; i < caffetteria.length; i++) {
-    document.getElementById(
-      "menù"
-    ).innerHTML += `
+  document.getElementById(
+    "caffetteria"
+  ).innerHTML += `
     <img class="img-fluid" src="${caffetteria[i]["img"]}" alt="${caffetteria[i]["nome"]}" />
     <div>${caffetteria[i]["nome"]}</div>
     `;
-  }
+}
+
+for (i = 0; i < gastronomia.length; i++) {
+  document.getElementById(
+    "gastronomia" //qui metto l'id
+  ).innerHTML += `
+    <img class="img-fluid" src="${gastronomia[i]["img"]}" alt="${gastronomia[i]["nome"]}" />
+    <div>${gastronomia[i]["nome"]}</div>
+    <div>${gastronomia[i]["prezzo"]}</div>
+    `;
+}
