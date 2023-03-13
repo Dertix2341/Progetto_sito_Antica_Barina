612163807ceb0b4670f12f7b205377a6ef2a60e6
Dertix2341 <dentino2416@gmail.com>
2023-03-13 22:53:34 +0100

    i fix the hader end i try some js

--- js/javascript.js
+++ js/javascript.js
@@ -1,14 +1,18 @@
 console.log("miao");
 
+//questo contenitore riempie la caffeteria cioè div con id="caffeteria"
 for (i = 0; i < caffetteria.length; i++) {
-  document.getElementById(
+  document.getElementById(//vai a prendere il documento ovvero l'html con id 
     "caffetteria"
   ).innerHTML += `
-    <img class="img-fluid" src="${caffetteria[i]["img"]}" alt="${caffetteria[i]["nome"]}" />
-    <div>${caffetteria[i]["nome"]}</div>
+    <div class="card_container">
+      <img class="img-fluid" src="${caffetteria[i]["img"]}" alt="${caffetteria[i]["nome"]}" />
+      <div>${caffetteria[i]["nome"]}</div>
+    </div>
     `;
 }
 
+//questo contenitore riempie la gastronomia cioè div con id="gastronimia"
 for (i = 0; i < gastronomia.length; i++) {
   document.getElementById(
     "gastronomia" //qui metto l'id
@@ -17,4 +21,15 @@ for (i = 0; i < gastronomia.length; i++) {
     <div>${gastronomia[i]["nome"]}</div>
     <div>${gastronomia[i]["prezzo"]}</div>
     `;
+}
+
+
+const array = [//array inizia a contare da 0
+  1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+]
+
+console.log(array.length);
+
+for (i = 1; i <= array.length; i++) {
+  console.log("indice array ", i - 1, "-> valore", array[i - 1]);
 }
