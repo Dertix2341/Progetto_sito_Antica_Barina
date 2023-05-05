13050c23809f43b4e13a70980688fdde2f2afd24
Dertix2341 <dentino2416@gmail.com>
2023-05-05 21:41:04 +0200

    commit for nothing

--- js/javascript.js
+++ js/javascript.js
@@ -35,23 +35,39 @@ for (i = 24; i < 37; i++) {
 }
 
 //questo contenitore riempie la gastronomia cioè div con id="gastronimia"
-for (i = 0; i < gastronomia.length; i++) {
+for (i = 0; i < 7; i++) {
   document.getElementById(
     "gastronomia" //qui metto l'id
   ).innerHTML += `
-    <img class="img-fluid" src="${gastronomia[i]["img"]}" alt="${gastronomia[i]["nome"]}" />
     <div>${gastronomia[i]["nome"]}</div>
     <div>${gastronomia[i]["prezzo"]}</div>
     `;
 }
 
+for (i = 7; i < 14; i++) {
+  document.getElementById(
+    "gastronomia2" //qui metto l'id
+  ).innerHTML += `
+    <div>${gastronomia[i]["nome"]}</div>
+    <div>${gastronomia[i]["prezzo"]}</div>
+    `;
+}
+
+for (i = 14; i < 21; i++) {
+  document.getElementById(
+    "gastronomia3" //qui metto l'id
+  ).innerHTML += `
+    <div>${gastronomia[i]["nome"]}</div>
+    <div>${gastronomia[i]["prezzo"]}</div>
+    `;
+}
 
 const array = [//array inizia a contare da 0
   1, 2, 3, 4, 5, 6, 7, 8, 9, 10
 ]
 
-console.log(caffetteria.length);
+console.log(gastronomia.length);
 
-for (i = 1; i <= caffetteria.length; i++) {
-  console.log("indice caffetteria ", i, "-> valore", array[i]);
+for (i = 1; i <= gastronomia.length; i++) {
+  console.log("indice gastronomia ", i, "-> valore", array[i]);
 }
